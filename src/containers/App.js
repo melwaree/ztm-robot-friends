0d61c54c99dba12919e8b7afe4e20a4b76c10041
Melina Mora <melinamora42@gmail.com>
2024-04-11 21:37:05 +0200

    Use React hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import GithubCorner from 'react-github-corner';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchField: '',
-        }
-    }
+function App() {
+    const [robots, setRobots] = useState([]);
+    const [searchField, setSearchField] = useState('');
 
-    componentDidMount(){
+    useEffect(()=>{
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=>{
             return response.json();
         })
         .then(robots=>{
-            this.setState({robots: robots});
+            setRobots(robots);
         })
-    }
+    },[]);
 
-    filterRobots(){
-        const { robots, searchField } = this.state;
+    const filterRobots = ()=>{
+        //const { robots, searchField } = this.state;
         return robots.filter(robot=>{
             return robot.name.toLowerCase().includes(searchField.toLowerCase());
         });
     }
 
-    onSearchChange= (event) => {
-        this.setState({searchField: event.target.value});
+    const onSearchChange = (event) => {
+        setSearchField(event.target.value);
     }
-
-    render() {
-        const filteredRobots = this.filterRobots();
-        
-        return !this.state.robots.length ? 
+    const filteredRobots = filterRobots();
+    
+        return !robots.length ? 
         <h1>Loading...</h1> : 
         (            
             <div className="tc">
@@ -47,13 +40,13 @@ class App extends Component {
                 <div id="stars3"></div>
                 <GithubCorner href="https://github.com/melwaree"/>
                 <h1 className="effect-shine">Robofriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <h6 className="effect-shine">Now with React hooks!</h6>
+                <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <CardList robots={filteredRobots} />
                 </Scroll>
             </div>
         );
-     }
 }
 
 
